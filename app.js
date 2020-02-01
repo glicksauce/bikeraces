f5704d2cd1126706754afc40b3d36de5f90c567a
Jay Glickman <jayglickman@gmail.com>
2020-02-01 14:11:16 -0500

    testing api calls from github pages site

--- app.js
+++ app.js
@@ -3,4 +3,28 @@ console.log("up and running")
 
 
 
+
+//$('form').on('submit',(event)=>{
+    //event.preventDefault();
+
+    //const userInput = $('input[type="text"]').val()
+    //numberComplaints = userInput
+
+    //console.log(userInput)
+
+    const promise = $.ajax({
+        url:'http://www.BikeReg.com/api/search'}).then(
+            (data)=>{
+                //console.log(JSON.stringify(new Date()))
+                console.log(data)
+                
+            },
+        ()=>{
+            console.log('bad request')
+            console.log("Error: " + data.statusText)
+        }
+    )
 })
+
+
+//})
